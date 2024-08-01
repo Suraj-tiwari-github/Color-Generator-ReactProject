ce2454b20f4fa3835b576bb697547e1e40d2ac74
PC <surajtiwari1155@gmail.com>
2024-08-01 23:37:50 +0530

    Added copy to clipboard

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,33 +1,55 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  const [color, setColor]=useState('');
-  const [error, setError]=useState(false);
-  const [list, setList]=useState([]);
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#f15025').all(10));
 
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    // console.log("hello");
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+      setList(colors);
+      setError(false);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
 
-  }
-
-
-  return <>color generator project
-  <section className="container">
-    <h3>Color Generator</h3>
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={color} onChange={(e)=>setColor(e.target.value)} placeholder='#f15025'/>
-      <button type='submit' className='btn'>Submit</button>
-    </form>
-  </section>
-
-  <section className="color">
-    <h3>List Goes here</h3>
-  </section>
-  </>
+  return (
+    <>
+      {/* color generator project */}
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? "error" : null}`}
+          />
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <h3 style={{}}>List Goes here</h3> 
+        {list.map((color,index)=>{
+          const hex=color.hex
+          console.log(color)
+          return <SingleColor key={index} {...color} index={index} hexColor={color.hex}/>
+        })}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
